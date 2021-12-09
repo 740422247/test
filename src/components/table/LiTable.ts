5cb07efbdb050843891acea4ec40a66f3de8719d
李兴星 <lixingxing@maytek.cn>
2021-12-09 10:08:48 +0800

    aaa

---
+++ src/components/table/LiTable.ts
@@ -0,0 +1,10 @@
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  setup() {
+    const msg = "this is table"
+    return {
+      msg,
+    }
+  },
+})
---
+++ src/components/table/LiTable.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="red">{{ msg }}</div>
+</template>
+<script>
+import "./LiTable.css"
+import LiTable from "@/components/table/LiTable.ts"
+export default LiTable
+</script>
