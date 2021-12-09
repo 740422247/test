19b069917fbb139159b7e1ea68e4c81f60194f60
李兴星 <lixingxing@maytek.cn>
2021-12-09 10:20:37 +0800

    bbb

--- src/components/table/LiTable.ts
+++ src/components/table/LiTable.ts
@@ -1,10 +1,16 @@
-import { defineComponent } from "vue"
+import { defineComponent } from "vue";
 
 export default defineComponent({
   setup() {
-    const msg = "this is table"
+    const msg = "this is table";
+    const aaa = () => {
+ console.log(111) 
+}
+
+    };
+
     return {
       msg,
-    }
+    };
   },
-})
+});
